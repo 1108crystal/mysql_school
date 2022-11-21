adbffea74d275594693edd25e2b21997852de145
crystal <1108crystal.gmail.com>
2022-11-21 11:09:06 +0800

    點選班級顯示表頭

--- index.php
+++ index.php
@@ -62,16 +62,20 @@ $sql_students = $sql_students . "LIMIT $start,$div";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>學生成績管理系統</title>
     <!-- 載入css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/css/bootstrap.min.css" integrity="sha512-CpIKUSyh9QX2+zSdfGP+eWLx23C8Dj9/XmHjZY2uDtfkdLGo0uY12jgcnkX9vXOgYajEKb/jiw67EYm+kBf+6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <h1>學生成績管理系統</h1>
+    <hr>
     <nav>
         <a href="add.php">新增學生</a>
         <a href="reg.php">教師註冊</a>
@@ -85,13 +89,9 @@ $sql_students = $sql_students . "LIMIT $start,$div";
             $sql = "SELECT `id`,`code`,`name` FROM `classes`";
             $classes = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
             foreach ($classes as $row) {
-                echo "<li><a href='?code={$row['code']}'>{$row['name']}</a></il>";
+                $nowclass=($_GET['code'] == $row['code'])?'nowclass':'';
+                echo "<li><a href='?code={$row['code']}'  class='$nowclass'>{$row['name']}</a></il>";
 
-                // if ($_GET['code'] = $row['code']) {
-                //     $classesName = $row['name'];
-                // } else {
-                //     $classesName = '';
-                // }
             }
             ?>
         </ul>
@@ -111,60 +111,125 @@ $sql_students = $sql_students . "LIMIT $start,$div";
     // echo "</pre>";
     ?>
 
-    <div class="pages">
-        <?php
-        //上一頁
-        //當前頁碼-1,可是不能小於0,最小是1,如果是0,不顯示
-        if (($now - 1) >= 1) {
-            $prev = $now - 1;
-            if (isset($_GET['code'])) {
-                echo "<a href='?page=$prev&code={$_GET['code']}'> ";
-                echo "&lt; ";
-                echo " </a>";
-            } else {
 
-                echo "<a href='?page=$prev'> ";
-                echo "&lt; ";
-                echo " </a>";
+
+<div class="pages">
+    <?php
+    //上一頁
+    //當前頁碼-1,可是不能小於0,最小是1,如果是0,不顯示
+    if(($now-1)>=1){
+        $prev=$now-1;
+        if(isset($_GET['code'])){
+            echo "<a href='?page=$prev&code={$_GET['code']}'> ";
+            echo "&lt; ";
+            echo " </a>";
+            
+        }else{
+   
+            echo "<a href='?page=$prev'> ";
+            echo "&lt; ";
+            echo " </a>";
+        }
+    }
+
+    ?>
+    <?php
+        //顯示第一頁
+        if($now>=4){
+            if(isset($_GET['code'])){
+                echo "<a href='?page=1&code={$_GET['code']}'> ";
+                echo "1 ";
+                echo " </a>...";
+                
+            }else{
+       
+                echo "<a href='?page=1'> ";
+                echo "1 ";
+                echo " </a>...";
             }
         }
+    ?>
+    <?php 
+    //頁碼區
+    //只顯示前後四個頁碼
 
-        ?>
-        <?php
-        //頁碼區
-        for ($i = 1; $i <= $pages; $i++) {
-            if (isset($_GET['code'])) {
-                echo "<a href='?page=$i&code={$_GET['code']}'> ";
+        if($now>=3 && $now<=($pages-2)){  //判斷頁碼在>=3 及小於最後兩頁的狀況
+            $startPage=$now-2;
+        }else if($now-2<3){ //判斷頁碼在1,2頁的狀況
+            $startPage=1;
+        }else{  //判斷頁碼在最後兩頁的狀況
+            $startPage=$pages-4;
+        }
+
+        for($i=$startPage;$i<=($startPage+4);$i++){
+            $nowPage=($i==$now)?'now':'';
+            if(isset($_GET['code'])){
+                echo "<a href='?page=$i&code={$_GET['code']}' class='$nowPage'> ";
                 echo $i;
                 echo " </a>";
-            } else {
-
-                echo "<a href='?page=$i'> ";
+                
+            }else{
+                echo "<a href='?page=$i' class='$nowPage'> ";
                 echo $i;
                 echo " </a>";
             }
         }
-        ?>
+
+
+    //全部頁碼顯示
+/*     for($i=1;$i<=$pages;$i++){
+        $nowPage=($i==$now)?'now':'';
+        if(isset($_GET['code'])){
+            echo "<a href='?page=$i&code={$_GET['code']}' class='$nowPage'> ";
+            echo $i;
+            echo " </a>";
+            
+        }else{
+            
+            echo "<a href='?page=$i' class='$nowPage'> ";
+            echo $i;
+            echo " </a>";
+        }
+    } */
+    ?>
         <?php
-        //下一頁
-        //當前頁碼+1,可是不能超過總頁數,最大是總頁數,如果超過總頁數,不顯示
-        if (($now + 1) <= $pages) {
-            $next = $now + 1;
-            if (isset($_GET['code'])) {
-                echo "<a href='?page=$next&code={$_GET['code']}'> ";
-                //echo "< ";
-                echo "&gt; ";
+        //顯示第一頁
+        if($now<=($pages-3)){
+            if(isset($_GET['code'])){
+                echo "...<a href='?page=$pages&code={$_GET['code']}'> ";
+                echo "$pages";
                 echo " </a>";
-            } else {
-                echo "<a href='?page=$next'> ";
-                //echo " >";
-                echo "&gt; ";
+                
+            }else{
+       
+                echo "...<a href='?page=$pages'> ";
+                echo "$pages";
                 echo " </a>";
             }
         }
+    ?>
+    <?php
+    //下一頁
+    //當前頁碼+1,可是不能超過總頁數,最大是總頁數,如果超過總頁數,不顯示
+    if(($now+1)<=$pages){
+        $next=$now+1;
+        if(isset($_GET['code'])){
+            echo "<a href='?page=$next&code={$_GET['code']}'> ";
+            //echo "< ";
+            echo "&gt; ";
+            echo " </a>";
+        }else{
+            echo "<a href='?page=$next'> ";
+            //echo " >";
+            echo "&gt; ";
+            echo " </a>";
+        }
+    }
 
-        ?>
-    </div>
+    ?>    
+</div>
+
+
 
 
     <table class="list-students">
@@ -172,11 +237,11 @@ $sql_students = $sql_students . "LIMIT $start,$div";
         <?php
         echo "<tr>";
 
-        echo "<td colspan=8 >{$classesName}</td>";
+        echo "<th colspan=8 >{$classesName}</th>";
 
         echo "</tr>";
 
-        echo "<tr>";
+        echo "<tr class='table-primary'>";
         echo "<td>學號</td>";
         echo "<td>姓名</td>";
         echo "<td>生日</td>";

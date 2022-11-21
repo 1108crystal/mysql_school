bf6c7eb4276e4abc950607a0320f0e58080d6b96
crystal <1108crystal.gmail.com>
2022-11-21 14:13:07 +0800

    刪除會員確認是否刪除?

--- api/del_student.php
+++ api/del_student.php
@@ -2,16 +2,23 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,'root','');
 
-$student=$pdo->query("select * from `students` where `id`='{$_GET['id']}'")->fetch(PDO::FETCH_ASSOC);
+//echo $_GET['id'];
+$student=$pdo->query("SELECT * FROM `students` where `id`='{$_GET['id']}'")
+             ->fetch(PDO::FETCH_ASSOC);
+$sql_class="DELETE FROM `class_student` WHERE `school_num`='{$student['school_num']}'";
+$sql_student="DELETE FROM `students` WHERE `id`='{$_GET['id']}'";
 
-$sql_class="delete from class_student where `school_num`='{$student['school_num']}'";
+echo $sql_class;
+echo "<br>";
+echo $sql_student;
+echo "<br>";
+//$pdo->query($sql);
+$res_class=$pdo->exec($sql_class);
+$res_student=$pdo->exec($sql_student);
+echo $res_class;
+echo "<br>";
+echo $res_student;
+echo "刪除成功:";
 
-$sql="delete from  `students` where `id`='{$_GET['id']}' ";
-echo $sql_class."<br>";
-echo $sql."<br>";
-// $pdo->query($sql);
-// $res=$pdo->exec($sql);
-
-echo "新增成功:".$res;
-
+header("location:../index.php?del=已成功刪除學生{$student['name']}的所有資料！！");
 ?>

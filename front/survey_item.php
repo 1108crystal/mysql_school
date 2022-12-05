e316f032daa1e4f089c2b0cde23ea54da26d5461
crystal <1108crystal.gmail.com>
2022-12-05 16:36:02 +0800

    問卷- 投票 & 投票結果

--- front/survey_item.php
+++ front/survey_item.php
@@ -1,30 +1,37 @@
-<h3 class="text-center font-weight-bold">Lorem ipsum dolor sit</h3>
+<?php
+if (isset($_GET['id'])) {
+    $subject = find('survey_subject', $_GET['id']);
+    //dd($subject);
+    $options = all('survey_options', ['subject_id' => $_GET['id']]);
+    //dd($options);
+} else {
+    header("location:admin_center.php?do=survey&error=沒有指定調查id");
+}
+?>
+
+<h3 class="text-center font-weight-bold"><?= $subject['subject']; ?></h3>
 
 <form action="./api/survey_vote.php">
-<div class="col-8 mx-auto mt-4">
-    <div class="input-group" style="margin-top:-1px">
-        <div class="input-group-prepend">
-            <div class="input-group-text">
-                <input type="radio" name="option">
+    <div class="col-8 mx-auto mt-4">
+        <?php
+        foreach ($options as $idx => $option) {
+        ?>
+            <div class="input-group" style="margin-top:-1px">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">
+                        <input type="radio" name="option">
+                    </div>
+                </div>
+                <div class="form-control">
+                    <?= $option['opt']; ?>
+                </div>
             </div>
-        </div>
-        <div class="form-control">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
-    </div>   
-    <div class="input-group" style="margin-top:-1px">
-        <div class="input-group-prepend">
-            <div class="input-group-text">
-                <input type="radio" name="option">
-            </div>
-        </div>
-        <div class="form-control">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </div>
-    </div>   
-</div>
-<div class="text-center mt-4">
-    <input type="submit" class="btn btn-primary mx-1" value="投票">
-    <a href="index.php?do=survey" class="btn btn-warning mx-1">取消返回</a>
-</div>
+        <?php
+        }
+        ?>
+    </div>
+    <div class="text-center mt-4">
+        <input type="submit" class="btn btn-primary mx-1" value="投票">
+        <a href="index.php?do=survey" class="btn btn-warning mx-1">取消返回</a>
+    </div>
 </form>
--- front/survey_result.php
+++ front/survey_result.php
@@ -1,18 +1,33 @@
-<h3 class="text-center font-weight-bold">調查結果</h3>
+<?php
+if (isset($_GET['id'])) {
+    $subject = find('survey_subject', $_GET['id']);
+    //dd($subject);
+    $options = all('survey_options', ['subject_id' => $_GET['id']]);
+    //dd($options);
+} else {
+    header("location:admin_center.php?do=survey&error=沒有指定調查id");
+}
+?>
+
+<h3 class="text-center font-weight-bold">【<?= $subject['subject']; ?> 】調查結果</h3>
 
 <ul class="list-group col-10 mx-auto">
-    <li class="d-flex list-group-item list-group-item-light list-group-item-action">
-        <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        <div class="col-6">
-            <div class="bg-primary rounded">&nbsp;</div>
-        </div>
-    </li>
-    <li class="d-flex list-group-item list-group-item-light list-group-item-action">
-        <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        <div class="col-6">
-            <div class="bg-primary rounded">&nbsp;</div>
-        </div>
-    </li>
+    <?php
+    foreach ($options as $idx => $option) {
+        $option['vote']; 
+    ?>
+        <li class="d-flex list-group-item list-group-item-light list-group-item-action">
+            <div class="col-6"> <?= $option['opt']; ?></div>
+            <div class="col-5">
+                <div class="bg-primary rounded">&nbsp;</div>
+            </div>
+            <div class="col-1">
+                <div ><?= $option['vote']; ?></div>
+            </div>
+        </li>
+    <?php
+    }
+    ?>
 </ul>
 <div class="text-center mt-4">
 

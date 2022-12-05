9e6d977af5382cc73284b4eb5b755a7d9a346b28
crystal <1108crystal.gmail.com>
2022-12-05 09:51:44 +0800

    測試調查主題寫入資料表

--- api/survey_add.php
+++ api/survey_add.php
@@ -1,6 +1,18 @@
 <?php
 include_once "../db/base.php";
 
+/*$subject=$_POST['subject'];
+$type= 1;
+$vote=0;
+$active=0;*/
+$subject=['subject'=>$subject,
+'type'=>$type,
+'ovte'=>$vote,
+'active'=>$active];
+
+insert('survey_subject',$subject);
+
+// insert('survey_options');
 
 
 
--- front/main.php
+++ front/main.php
@@ -1,8 +1,12 @@
 <h1 class="text-center">最新消息</h1>
 <ul class="list-group">
 <li class='list-group-item list-group-item-action d-flex text-center bg-info text-white'>
-    <div class='col-md-10'>標題</div>
-    <div class='col-md-2'>人氣</div>
+
+    <div class='col-md-1'></div>
+            <div class='col-md-1'>類別</div>
+            <div class='col-md-6'>主題</div>
+            <div class='col-md-2'>點閱數</div>
+            <div class='col-md-2'>發佈時間</div>
 </li>    
 <?php
 
@@ -14,16 +18,22 @@ $rows=all('news'," ORDER by `top` desc,`readed` desc");
 //$hot=$pdo->query("SELECT `id` FROM `news` ORDER BY `readed` desc")->fetchColumn();
 $hot=q("SELECT `id` FROM `news` ORDER BY `readed` desc");
 foreach($rows as $row){
-    echo "<li class='list-group-item list-group-item-action d-flex'>";
-    echo "<div class='col-md-10'>";
-    echo ($row['top']==1)?"top":'';
-    echo ($row['id']==$hot)?"hot":'';
-    echo "<a href='index.php?do=news_detail&id={$row['id']}'>";
-    echo $row['subject'];
-    echo "</a>";
+    echo "<li class='list-group-item list-gropu-item-action d-flex'>";
+    echo "<div class='col-md-1 text-danger'>";
+    echo ($row['top'] == 1) ? "TOP" : "";
+    echo ($row['id'] == $hot) ? "hot!" : "";
     echo "</div>";
-    echo "<div class='col-md-2 text-center'>";
-    echo $row['readed'];
+    echo "<div class='col-md-1'>";
+    echo $row['type'];
+    echo "</div>";
+    echo "<div class='col-md-6'>";
+    echo "<a href='index.php?do=news_detail&id={$row['id']}'>{$row['subject']}</a>";
+    echo "</div>";
+    echo "<div class='col-md-1 text-center'>";
+    echo "<a class='btn btn-info mx-1' href='index.php?do=news_detail&id={$row['id']}'>{$row['readed']}</a>";
+    echo "</div>";
+    echo "<div class='col-md-3 text-center'>";
+    echo "{$row['created_at']}";
     echo "</div>";
     echo "</li>";
 }
@@ -31,3 +41,5 @@ foreach($rows as $row){
 ?>
 
 </ul>
+
+

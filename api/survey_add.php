2886d0174ab5a77ebdb9a4ada39ecb9bd8b3d1fa
crystal <1108crystal.gmail.com>
2022-12-05 10:49:33 +0800

    後台新增問巻功能

--- api/survey_add.php
+++ api/survey_add.php
@@ -5,15 +5,25 @@ include_once "../db/base.php";
 $type= 1;
 $vote=0;
 $active=0;*/
-$subject=['subject'=>$subject,
-'type'=>$type,
-'ovte'=>$vote,
-'active'=>$active];
 
+$subject=['subject'=>$_POST['subject'],
+'type'=>1,
+'vote'=>0,
+'active'=>0];
+dd($subject);
 insert('survey_subject',$subject);
 
-// insert('survey_options');
+$subject_id=find('survey_subject',['subject'=>$_POST['subject']])['id'];
 
-
-
-?>
+if(isset($_POST['opt'])){
+    foreach($_POST['opt'] as $option){
+        if($option!=''){
+        $tmp=['opt'=>$option,
+               'subject_id'=>$subject_id,
+               'vote'=>0];
+               dd($tmp);
+               insert('survey_options',$tmp);
+            }
+    }
+}
+header("location:../admin_center.php?do=survey");
